5d9b9a8ae90fa5290925119de3b855a9d41d597b
IT24100452 <it24100452@my.sliit.lk>
2024-08-21 09:24:40 -0700

    Add IT24100452Lab5Q2.java to Q2

---
+++ Q2/IT24100452Lab5Q2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class IT24100452Lab5Q2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of new members introduced: ");
+        int members = input.nextInt();
+
+        if (members < 0) {
+            System.out.println("Input must be a number 0 or greater.");
+            return;
+        }
+
+        String prize;
+
+        switch (members) {
+            case 0:
+                prize = "No Prize";
+                break;
+            case 1:
+                prize = "Pen";
+                break;
+            case 2:
+                prize = "Umbrella";
+                break;
+            case 3:
+                prize = "Bag";
+                break;
+            case 4:
+                prize = "Travelling Chair";
+                break;
+            default:
+                prize = "Headphone";
+                break;
+        }
+
+        System.out.println("Prize is a: " + prize);
+    }
+}
